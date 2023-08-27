17e945310086ac436db7c63987afba1f575b0481
Link <link@Links-MacBook-Pro.local>
2023-08-27 15:39:45 -0500

    Upload projects

---
+++ CNN-MNIST/main.py
@@ -0,0 +1,249 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Adjust the model to get a higher performance
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3, 1)
+        self.conv2 = nn.Conv2d(8, 16, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(2304, 64)
+        self.fc2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        return x
+# Add addtional two convolutional layers to the model
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3, 1)
+        self.conv2 = nn.Conv2d(8, 16, 3, 1)
+        self.conv3 = nn.Conv2d(16, 16, 3, 1)
+        self.conv4 = nn.Conv2d(16, 32, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(2048, 64)
+        self.fc2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        return x
+
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    plt.figure()
+    pic = None
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if batch_idx in (1,2,3,4,5):
+            if batch_idx == 1:
+                pic = data[0,0,:,:]
+            else:
+                pic = torch.cat((pic,data[0,0,:,:]),dim=1)
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        # Calculate gradients
+        loss.backward()
+        # Optimize the parameters according to the calculated gradients
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
+    #plt.imshow(pic.cpu(), cmap='gray')
+    #plt.show()
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+# Function to display the images in the test set
+def vis_mnist(model, device, test_loader):
+    classes = list(range(10))
+    images, labels = next(iter(test_loader))
+    images = images.to(device)
+    labels = labels.to(device)
+    test_output = model(images)
+    test_output = F.softmax(test_output, dim=1)
+    val_probs,val_preds = torch.max(test_output,1)
+
+
+    correct = torch.sum(val_preds == labels.data).item()
+    val_preds = val_preds.cpu()
+    labels = labels.cpu()
+    print("Accuracy Rate = {}%".format(correct/len(images) * 100))
+
+    fig = plt.figure(figsize=(25,25))
+    # some random training images
+    for idx in range(25):    
+        ax = fig.add_subplot(5, 5, idx+1, xticks=[], yticks=[])
+        plt.imshow(np.squeeze(images[idx].cpu()))
+        ax.set_title("{},conf={:.2f} ({})".format(classes[val_preds[idx].item()], val_probs[idx].item(), classes[labels[idx].item()]), 
+                    color = ("green" if val_preds[idx].item()==labels[idx].item() else "red"), fontsize = 10)
+    # some misclassified images
+    # count = 0
+    # for idx in range(999):
+    #     if val_preds[idx].item() != labels[idx].item():
+    #         count += 1
+    #         # print("Prediction: {}, Confidence: {:.2f} ({})".format(classes[val_preds[idx].item()], val_probs[idx].item(), classes[labels[idx].item()]))
+    #         ax = fig.add_subplot(5, 5, count, xticks=[], yticks=[])
+    #         plt.imshow(np.squeeze(images[count].cpu()))
+    #         ax.set_title("{},conf={:.2f} ({})".format(classes[val_preds[idx].item()], val_probs[idx].item(), classes[labels[idx].item()]), 
+    #                     color = ("green" if val_preds[idx].item()==labels[idx].item() else "red"), fontsize = 10)
+    plt.show()
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # batch_size is a crucial hyper-parameter
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
+    if use_cuda:
+        # Adjust num worker and pin memory according to your computer performance
+        cuda_kwargs = {'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    # Normalize the input (black and white image)
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+    # Make train dataset split
+    dataset1 = datasets.MNIST('./data', train=True, download=True,
+                       transform=transform)
+    # Make test dataset split
+    dataset2 = datasets.MNIST('./data', train=False,
+                       transform=transform)
+
+    # Convert the dataset to dataloader, including train_kwargs and test_kwargs
+    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+    # Put the model on the GPU or CPU
+    model = Net().to(device)
+
+    # Create optimizer
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    # Create a schedule for the optimizer
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+
+    # Begin training and testing
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+    vis_mnist(model, device, test_loader)
+    # Save the model
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+        
+    model = Net2().to(device)
+    
+    # Create optimizer
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    # Create a schedule for the optimizer
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    
+    # Begin training and testing
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+    vis_mnist(model, device, test_loader)
+        
+    # Save the model
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn2.pt")
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,9 @@
+# 👨🏻‍💻  Analytical Project Experience
+The collection of the projects related to data analytics and machine learning.
+- Data Scrap
+  + [Data Sracping and Analysis for a Book Website](https://github.com/kazam1/Data-Analysis-Project/blob/main/Data_Scraping_and_Analysis_For_Book_Website.ipynb)
+- Machine Learning (Python)
+  + [EDA and XGBoost Prediction for Employee Attrition](https://github.com/kazam1/Data-Analysis-Project/blob/main/EDA_and_XGBoost_Prediction_For_Employee_Attrition%20.ipynb)
+- Deep Learning
+  + [NLP-based Analysis for Chinese Crowdworkers](https://github.com/Linkkk01/Data-Analysis-Project/tree/main/NLP-based%20Analysis%20for%20Chinese%20Crowdworkers)
+  + [CNN for Hand Writing Recognition on MNIST](https://github.com/Linkkk01/Data-Analysis-Project/tree/main/CNN-MNIST)
